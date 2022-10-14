1865b34401d8931bddb22c04af48581e9cd367e0
sbcomputertech <sbcomputertech.0@gmail.com>
2022-10-14 21:40:57 +0100

    Added mod list in pause menu

--- BepInEx.Core/Cobweb/MultiplayerPatches.cs
+++ BepInEx.Core/Cobweb/MultiplayerPatches.cs
@@ -55,4 +55,45 @@ namespace BepInEx.Core.Cobweb
             return false;
         }
     }
+
+    [HarmonyPatch("VersionNumberTextMesh", "Start")]
+    internal class CobwebPatch_VersionText
+    {
+        private static bool hasDoneTextPatch = false;
+
+        [HarmonyPostfix]
+        public static void AddText(ref object __instance)
+        {
+            if(!hasDoneTextPatch)
+            {
+                var textMeshInfo = __instance.GetType().GetField("textMesh");
+                var textMesh = textMeshInfo.GetValue(__instance);
+
+                //Logger.Log(LogLevel.Warning, "TextMesh via reflecttion: " + textMesh.ToString());
+
+                var setTextInfo = textMesh.GetType().GetMethods().Where(m => m.Name == "SetText").Where(m => m.GetParameters().Length == 1).First();
+
+                var currentText = (string)textMesh.GetType().GetProperty("text").GetValue(textMesh, null);
+                StringBuilder sb = new StringBuilder(currentText);
+                sb.Append("\n\nMods:");
+
+                foreach (var plugin in Util.Plugins)
+                {
+                    var name = plugin.Value.Metadata.Name;
+                    var version = plugin.Value.Metadata.Version;
+
+                    sb.Append("\n- ");
+                    sb.Append(name);
+                    sb.Append(" v");
+                    sb.Append(version);
+                }
+
+                setTextInfo.Invoke(textMesh, new object[] { sb });
+
+                textMeshInfo.SetValue(__instance, textMesh);
+
+                hasDoneTextPatch = true;
+            }
+        }
+    }
 }
